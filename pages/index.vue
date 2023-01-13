219df3f4763b079aa1d757186cd9762374eb2a1d
Abigail <ashbella333@gmail.com>
2023-01-13 16:39:06 +0000

    added input field for adding the todos

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,18 @@
     <main>
       <h1>Task Board</h1>
       <p>Create a list of todos</p>
+      <div>
+        <input type="text" v-model="newTodo" placeholder="add a new todo" @keypress.enter="addTodo" class="border-4 border-indigo-600" />
+        <button @click="addTodo">add</button>
+      </div>
+
+
+      <div> 
+        <!----component-->
+      </div>
+      
     </main>
 
-    <div>
-      <input type="text" v-model="newTodo" placeholder="add a new todo" @keypress.enter="addTodo" class="border-4 border-indigo-600" />
-      <button @click="addTodo">add</button>
-    </div>
-    
   </div>
 </template>
 
