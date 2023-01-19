c31ba44c500300a1a7aff3d0f208cfb0b49af8e5
Abigail <ashbella333@gmail.com>
2023-01-19 15:30:04 +0000

    added new svg image

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
-    <main>
-      <h1>Task Board</h1>
-      <p>Create a list of todos</p>
-      <div>
-        <input type="text" v-model="newTodo" placeholder="add a new todo" @keypress.enter="addTodo" class="border-4 border-indigo-600" />
-        <button @click="addTodo">add</button>
-      </div>
+    <main class=" h-screen">
+      <div class="flex justify-center"> 
+
+        <div class=" ">
+          <img src="images/todo.svg.svg" alt="PanCake" class=" object-cover " />
+        
+        </div>
+
+        
+        <!-- <div> 
+          <h1>Task Board</h1>
+          <p>Create a list of todos</p>
+
+        </div> -->
+
+        <div>
+          <input type="text" v-model="newTodo" placeholder="add a new todo" @keypress.enter="addTodo" class="border-4 border-indigo-600" />
+          <button @click="addTodo">add</button>
+        </div>
+  
+  
 
 
-      <div> 
-        <!----component-->
       </div>
       
     </main>
